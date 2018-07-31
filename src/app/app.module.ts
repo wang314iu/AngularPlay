f8061bdf603c8203d0cb3b3a4e6ea66139b994e3
Reddy57 <abhilash.gaddam@antra.net>
2018-07-31 12:48:13 -0400

    added basic files required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,30 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ApiService } from './services/api.service';
 import { MovieListComponent } from './movies/movie-list.component';
+import { MovieComponent } from './movies/movie.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './shared/components/not-found.component';
+import { HeaderComponent } from './shared/layout/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovieListComponent
+    MovieListComponent,
+    MovieComponent,
+    HomeComponent,
+    NotFoundComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'movies', component: MovieListComponent },
+      { path: 'movie/:id', component: MovieComponent },
+      { path: '**', component: NotFoundComponent }
+    ])
 
   ],
   providers: [ApiService],
