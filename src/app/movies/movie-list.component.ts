8afbbf39187552f6ee5b4a2ae2a51c25c50750aa
Reddy57 <abhilash.gaddam@antra.net>
2018-08-01 14:42:36 -0400

    added movies by genre

--- src/app/movies/movie-list.component.ts
+++ src/app/movies/movie-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../services/movie.service';
 import { Movie } from '../shared/models/movie';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-list',
@@ -10,18 +11,40 @@ import { Movie } from '../shared/models/movie';
 export class MovieListComponent implements OnInit {
 
   movies: Movie[];
+  gid: number;
+  urlsegmant: string;
 
-  constructor(private movieService: MovieService) { }
+  constructor(private movieService: MovieService, private route: ActivatedRoute) { }
 
   ngOnInit() {
 
-    this.movieService.getAllMovies()
-      .subscribe(
-        m => {
-          this.movies = m;
-          console.log(this.movies);
+    this.route.paramMap.subscribe(
+      params => {
+        console.log(this.route.snapshot.url);
+        this.urlsegmant = this.route.snapshot.url[0].path;
+        this.gid = +params.get('id');
+
+        if (this.gid > 0) {
+          this.movieService.getMoviesByGenre(this.gid)
+            .subscribe(
+              g => {
+                this.movies = g;
+                //  console.log('movie card called');
+              }
+            );
+        } else {
+          this.movieService.getAllMovies()
+            .subscribe(
+              m => {
+                this.movies = m;
+                console.log(this.movies);
+              }
+            );
         }
-      );
+      }
+    );
+
+
   }
 
 }
